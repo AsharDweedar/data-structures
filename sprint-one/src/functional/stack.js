05f5913348d954b51745405d0e19a6875b6520b7
RBK25 <rbk25@RBK25s-Mac-mini.local>
2017-08-21 04:36:09 -0400

    finish the stack function

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,29 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size =0;
   // Implement the methods below
   someInstance.push = function(value) {
+    size++;
+    storage[''+size]=value;
+    console.log(storage)
   };
 
   someInstance.pop = function() {
+     
+
+     if (size !== 0)
+       {
+        var value = storage[''+size]
+         delete storage[''+size];
+          size--;
+       } 
+       return value;
+    
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
