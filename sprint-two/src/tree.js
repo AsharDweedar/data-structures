9193790f67981988bdd5e9f7d9148b5cab2d8474
maisaa <maisaa.alkhedr@gmail.com>
2017-08-23 12:30:48 -0400

    tree is ok too

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
   // your code here
-  newTree.children = null;  // fix me
+  newTree.addChild = treeMethods.addChild ;
+  newTree.contains = treeMethods.contains ;
+  // your code here
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +13,34 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+	var child = Tree(value);
+	this.children.push(child);	
 };
 
 treeMethods.contains = function(target) {
+	var search = function(node){
+	   if (node.value === target) {
+	   		return true;
+	   }
+	   for (var i = 0; i < node.children.length; i++) {
+		   	if (search (node.children[i]) ){
+		   		return true;
+		   	}
+	   }
+	   return false;
+
+	}
+	if(this.value === target){
+		return true ;
+	} else {
+		for (var i = 0; i < this.children.length; i++) {
+		   	if (search (this.children[i]) ){
+		   		return true;
+		   	}
+	   }
+	   return false
+	}
+
 };
 
 
@@ -21,3 +48,6 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+
+
