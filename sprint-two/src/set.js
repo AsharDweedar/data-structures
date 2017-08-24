e3183e768a46f483d9ba9fc9c415dca1a0a72384
ashardweedar <ashar.dweedar@gmail.com>
2017-08-24 03:19:34 -0400

    finish the set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	this._storage.push(item);
+
 };
 
 setPrototype.contains = function(item) {
+	if ( this.search(item) !== -1 ) {
+		return true;
+	}
+	return false;
 };
 
 setPrototype.remove = function(item) {
+	this._storage.splice(this.search(item), 1);
+};
+
+setPrototype.search = function(item) {
+	for (var i = 0; i < this._storage.length; i++) {
+		if (this._storage[i] === item) {
+			return i;
+		}
+	}
+	return -1;
 };
 
 /*
